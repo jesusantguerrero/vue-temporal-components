1f079123af7c8d972a4379ef8032d8aa46995179
Jesus Guerrero <jesusant.guerrero@gmail.com>
2022-04-10 11:43:50 -0400

    fix:(timer) add icons and update css

---
+++ src/components/atoms/IconSharpChevronRight.vue
@@ -0,0 +1,10 @@
+
+<template>
+  <svg width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M10 6L8.59 7.41L13.17 12l-4.58 4.59L10 18l6-6l-6-6z"></path></svg>
+</template>
+
+<script>
+export default {
+  name: 'IcSharpChevronRight'
+}
+</script>
