6b3dfdcb66691442318c7f5250f4b929ed1f0e09
Jesus Guerrero <jesusant.guerrero@gmail.com>
2023-01-14 00:00:52 -0400

    test: separate event tests

--- src/components/Timer/__tests__/timeTracker.spec.js
+++ src/components/Timer/__tests__/timeTracker.spec.js
@@ -14,36 +14,61 @@ describe("Timer component zen use cases", () => {
         disabled: true,
       },
     });
-
     let time = "";
     const btnPlay = getByTestId("btn-play");
-    expect(getByTestId("current-time").textContent).toBe("25:00");
+
     fireEvent.click(btnPlay);
     vi.advanceTimersByTime(2000);
     time = getByTestId("current-time").textContent;
     vi.clearAllTimers();
     fireEvent.click(getByTestId("btn-play"));
+
     expect(time).toBe("25:00");
     expect(queryByTestId("current-time")?.textContent).toBe("25:00");
   });
 
-  it("should emit events", async () => {
+  it("should emit start event", async () => {
     vi.useFakeTimers();
-    render(Timer, {
+    const { emitted } = render(Timer, {
+      props: {
+        task: tasks[0],
+      },
+    });
+    const btnPlay = screen.queryByTestId("btn-play");
+
+    await fireEvent.click(btnPlay);
+
+    expect(emitted()).toHaveProperty("started");
+  });
+
+  it("should emit tick event", async () => {
+    vi.useFakeTimers();
+    const { emitted } = render(Timer, {
+      props: {
+        task: tasks[0],
+      },
+    });
+    const btnPlay = screen.queryByTestId("btn-play");
+
+    await fireEvent.click(btnPlay);
+
+    vi.advanceTimersByTime(1000);
+    expect(emitted()).toHaveProperty("tick");
+    vi.clearAllTimers();
+  });
+  it("should emit stopped event", async () => {
+    vi.useFakeTimers();
+    const { emitted } = render(Timer, {
       props: {
         task: tasks[0],
       },
     });
 
     const btnPlay = screen.queryByTestId("btn-play");
-    expect(screen.getByTestId("current-time").textContent).toBe("25:00");
     await fireEvent.click(btnPlay);
-    // expect(emitted()).toHaveProperty("started");
+
     vi.advanceTimersByTime(1000);
-    // expect(emitted()).toHaveProperty("tick");
-    vi.clearAllTimers();
     await fireEvent.click(btnPlay);
-    expect(screen.getByTestId("current-time").textContent).toBe("25:00");
-    // expect(emitted()).toHaveProperty("stopped");
+    expect(emitted()).toHaveProperty("stopped");
   });
 });
